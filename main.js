5fe654c66e59d85db3c549ed5e61a61358f2debf
kawan <kawanssoares@gmail.com>
2024-09-04 08:27:07 -0300

    validacoes

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 const form = document.getElementById('formCampos');
-let formValido = false;
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     
-    const campoA = document.getElementById('campoA').value;
-    const campoB = document.getElementById('campoB').value;
-    const success = `O Número ${campoB} é <b>Maior</b> que ${campoA}`;
-    const error = `O Número ${campoB} é <b>Menor</b> que ${campoA}`;
-    const draw = `Ambos os Números (${campoA} e ${campoB}) são Iguais`;
+    const campoA = document.getElementById('campoA');
+    const campoB = document.getElementById('campoB');
+    const success = `O Número ${campoB.value} é <b>Maior</b> que ${campoA.value}`;
+    const error = `O Número ${campoB.value} é <b>Menor</b> que ${campoA.value}`;
+    const draw = `Ambos os Números (${campoA.value} e ${campoB.value}) são Iguais`;
 
-    if(campoA < campoB){
+    if(campoA.value < campoB.value){
         const mensagemSucesso = document.querySelector('.Message');
-        // document.querySelector('.successMessage').style.display='block'
         mensagemSucesso.innerHTML = success;
         mensagemSucesso.style.display='block';
-        mensagemDraw.style.backgroundColor='green';
-        campoA = '';
-        campoB = '';
-    }else if (campoA == campoB){
+        mensagemSucesso.style.backgroundColor='green';
+        campoA.value = '';
+        campoB.value = '';
+    }else if (campoA.value == campoB.value){
         const mensagemDraw = document.querySelector('.Message');
         mensagemDraw.innerHTML = draw;
         mensagemDraw.style.display='block';
         mensagemDraw.style.backgroundColor='red';
-        campoA = '';
-        campoB = '';
-    } else {
-        // document.querySelector('.errorMessage').style.display='block'
+        campoA.value = '';
+        campoB.value = '';
+    } 
+    else {
         const mensagemErro = document.querySelector('.Message');
         mensagemErro.innerHTML = error;
         mensagemErro.style.display='block';
         mensagemErro.style.backgroundColor='red';
-        campoA = '';
-        campoB = '';
+        campoA.value = '';
+        campoB.value = '';
     }
 })
+
+console.log(form)
