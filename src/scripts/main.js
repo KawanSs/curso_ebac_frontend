bc884cf69b0e0c31eb732ec4ea383ea19f58113b
kawan <kawanssoares@gmail.com>
2024-12-28 05:41:52 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const name = document.querySelector('#name');
+    const user = document.querySelector('#user');
+    const avatar = document.querySelector('#avatar');
+    const repositories = document.querySelector('#repositories');
+    const followers = document.querySelector('#followers');
+    const following  = document.querySelector('#following');
+    const link = document.querySelector('#link');
+    const endpoint = 'https://api.github.com/users/KawanSs';
+    
+    fetch(endpoint).then(function(res){
+            return res.json();
+        })
+        .then(function(json){
+            name.innerText = json.name;
+            user.innerText = json.login;
+            avatar.src = json.avatar_url;
+            repositories.innerText = json.public_repos;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+            link.href = json.html_url;
+        })
+})
